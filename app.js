7387b8163e4b29de693ea3fa9572fb3661c16ab4
Seb-mlrd <sebmlrd06@gmail.com>
2024-10-17 12:14:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const info = document.querySelector(".infos")
+const allBtns = document.querySelectorAll("input[type='button']")
+
+function val(resultat){
+    form.fenetre.value = form.fenetre.value + resultat
+}
+
+function calcule(){
+    if(form.fenetre.value  == ""){
+        info.innerText = "Veuillez écrire un chiffre"
+        allBtns.forEach(btn => {
+            btn.addEventListener("click", function (){
+                info.innerText = ""
+            })
+        })
+    }else (
+        form.fenetre.value = eval(form.fenetre.value)
+    )
+}
+ function suppr(){
+    form.fenetre.value = ""
+ }
